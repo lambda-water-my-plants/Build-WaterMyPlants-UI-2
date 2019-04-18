eaaf271c93e992ec73929753eed157b704271cb3
LVWood <leslee.wood@gmail.com>
2019-04-18 10:32:02 -0500

    replaced bootstrap cards with native code, tweaked styling to fit new components

---
+++ js/components.js
@@ -0,0 +1,26 @@
+class Title {
+    constructor(title) {
+        this.title = title;
+        this.content = document.querySelector(`.content[data-tab="${this.title.dataset.tab}"]`);
+        this.content = new Content(this.content);
+        this.title.addEventListener('click', () => this.titleClick());
+    }
+    titleClick() {
+        this.content.toggleContent();
+    };
+}
+
+
+class Content {
+    constructor(content) {
+        this.paragraph = content;
+    }
+    toggleContent() {
+        this.paragraph.classList.toggle('change');
+    }
+}
+
+const titles = document.querySelectorAll(".title");
+titles.forEach(function(title) {
+    return new Title(title);
+});
--- js/stylequeries.js
+++ js/stylequeries.js
@@ -1,46 +1,35 @@
-const button = document.querySelectorAll('.btn');
-button.forEach(function(button) {
-    button.style.color = "#FEAC88";
-    button.style.backgroundColor = " #233D4D";
-    button.style.fontFamily = "Yeseva One";
-});
+// const button = document.querySelectorAll('.anchorbutton');
+// button.forEach(function(button) {
+//     button.style.backgroundColor = "#FEAC88";
+//     button.style.color = " #233D4D";
+//     button.style.fontFamily = "Yeseva One";
+//     button.style.fontSize = "1.2rem";
+// });
 
-const card = document.querySelectorAll('.card-body');
-card.forEach(function(card) {
-    card.style.backgroundColor = ' #FCF9F4';
-});
+// const cta = document.querySelectorAll('.title');
+// cta.forEach(function(cta) {
+//     cta.style.fontSize = "2rem";
+//     fontFamily = "Yeseva One";
+// });
 
-const text = document.querySelectorAll('.card-text');
-card.forEach(function(text) {
-    text.style.fontFamiy = "Lora";
-    text.style.color = "#233D4D";
-});
+// // const intro = document.querySelector('#top-intro');
+// // intro.style.fontFamily = "Yeseva One";
+// // intro.style.color = "#FCF9F4";
+// // intro.style.fontSize = "2rem";
 
-const title = document.querySelectorAll('.card-title');
-title.forEach(function(title) {
-    title.style.fontFamily = "Yeseva One";
-    title.style.color = "#233D4D";
-    title.style.fontSize = "2rem";
-});
+// const introparagraph = document.querySelector('.about-paragraph');
+// introparagraph.style.fontFamily = "Lora";
+// introparagraph.style.color = "#FCF9F4";
 
-const intro = document.querySelector('#top-intro');
-intro.style.fontFamily = "Yeseva One";
-intro.style.color = "#FCF9F4";
-intro.style.fontSize = "2rem";
+// const bio = document.querySelectorAll('.bio');
+// bio.forEach(function(bio) {
+//     bio.style.fontFamily = "Lora";
+//     bio.style.color = "#233D4D";
+// });
 
-const introparagraph = document.querySelector('.about-paragraph');
-introparagraph.style.fontFamily = "Lora";
-introparagraph.style.color = "#FCF9F4";
-
-const bio = document.querySelectorAll('.bio');
-bio.forEach(function(bio) {
-    bio.style.fontFamily = "Lora";
-    bio.style.color = "#233D4D";
-});
-
-const name = document.querySelectorAll('.name');
-name.forEach(function(name) {
-    name.style.fontFamily = "Yeseva One";
-    name.style.color = "#233D4D";
-});
+// const name = document.querySelectorAll('.name');
+// name.forEach(function(name) {
+//     name.style.fontFamily = "Yeseva One";
+//     name.style.color = "#233D4D";
+// });
 
